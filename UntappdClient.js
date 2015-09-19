d3564437fbb8bb60f0925a8cc5a82adc3a9d4c63
Anthony Torres <am17torres@gmail.com>
2015-09-18 20:42:24 -0400

    remove client secret for first step in server auth

--- UntappdClient.js
+++ UntappdClient.js
@@ -155,15 +155,15 @@ var UntappdClient = function(debug) {
 	// this url can be used in the browser to get the access token
 	that.getUserAuthenticationURL = function(returnRedirectionURL) {
 		if (returnRedirectionURL===undefined || returnRedirectionURL===null) throw new Error("returnRedirectionURL cannot be undefined or null.");
-		if (!hasId() || !hasSecret()) throw new Error("UntappdClient.getUserAuthenticationURL requires a ClientId/ClientSecret pair.");
+		if (!hasId()) throw new Error("UntappdClient.getUserAuthenticationURL requires a ClientId/ClientSecret pair.");
 		return "https://untappd.com/oauth/authenticate/?client_id="+id+"&response_type=token&redirect_url="+returnRedirectionURL;
 	};
 
 	//this is for server-side, Step 1 - OAUTH Authentication
 	that.getAuthenticationURL = function(returnRedirectionURL){
 		if (returnRedirectionURL===undefined || returnRedirectionURL===null) throw new Error("returnRedirectionURL cannot be undefined or null.");
-		if (!hasId() || !hasSecret()) throw new Error("UntappdClient.getUserAuthenticationURL requires a ClientId/ClientSecret pair.");
-		return "https://untappd.com/oauth/authenticate/?client_id="+id+"&client_secret="+secret+"&response_type=code&redirect_url="+returnRedirectionURL+"&code=COD";
+		if (!hasId()) throw new Error("UntappdClient.getUserAuthenticationURL requires a ClientId/ClientSecret pair.");
+		return "https://untappd.com/oauth/authenticate/?client_id="+id+"&response_type=code&redirect_url="+returnRedirectionURL+"&code=COD";
 	};
 
 	// Step 2 - OATUH Authorization
@@ -482,4 +482,3 @@ var UntappdClient = function(debug) {
 };
 
 module.exports = UntappdClient;
-
